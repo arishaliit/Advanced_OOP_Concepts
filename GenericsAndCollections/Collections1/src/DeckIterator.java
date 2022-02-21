60afd2c34bb7fe401b61b71bb2dfec328903846e
arishaliit <arish.fxtm@gmail.com>
2022-02-21 08:59:59 -0600

    Finished All task in Generics & Collections Generics + Collections 1 + Collections 2

--- GenericsAndCollections/Collections1/src/DeckIterator.java
+++ GenericsAndCollections/Collections1/src/DeckIterator.java
@@ -1,22 +1,18 @@
 import java.util.Iterator;
 
-public class DeckIterator<E> /* TODO
-Implement the Iterator interface */ {
+public class DeckIterator<E> implements Iterator<E> {
     Deck d;
     public DeckIterator(Deck d){
-/* TODO
-initialize the variable d */
+        this.d = d;
     }
 
     @Override
     public boolean hasNext() {
-       /* TODO
-Return true if there are elements left in the Deck */
+       return d.size() > 0 ? true : false;
     }
 
     @Override
     public E next() {
-        /* TODO
-Deal a card and return it. */
+        return (E) d.dealCard();
     }
 }
--- GenericsAndCollections/Generics/src/BingoChip.java
+++ GenericsAndCollections/Generics/src/BingoChip.java
@@ -1,4 +1,14 @@
-public class BingoChip/* TODO 
-Implement this class.
-Use the instructions and check how it is used in Task.java*/
+public class BingoChip<V,S> {
+    V var1;
+    S var2;
+
+    public BingoChip(V var1, S var2){
+        this.var1 = var1;
+        this.var2 = var2;
+    }
+
+    public String toString(){
+        return "Chip:"+var1+"-"+var2;
+    }
+}
 
